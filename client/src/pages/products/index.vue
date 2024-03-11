2d3d3db64c0448259252b03eb2e902ec864661b3
youssup <Youssupsong@gmail.com>
2024-03-11 14:58:33 -0400

    Created Flyout Component

--- client/src/pages/products/index.vue
+++ client/src/pages/products/index.vue
@@ -2,6 +2,7 @@
     import { ref, computed } from 'vue'
     import { type Product, getProducts } from "@/model/products";
 import ProductCard from '@/components/ProductCard.vue';
+import FlyOut from '@/components/FlyOut.vue';
 
     const products = ref([] as Product[])
     
@@ -34,7 +35,7 @@ import ProductCard from '@/components/ProductCard.vue';
         <ProductCard v-for="product in products" :key="product.id" :product="product" @addToCart="addToCart"/>
     </div>
 
-    <div class="flyout">
+    <FlyOut :isOpen="true">
         <h1 class="title">
             The Cart
         </h1>
@@ -45,7 +46,7 @@ import ProductCard from '@/components/ProductCard.vue';
             </li>
         </ul>
         {{ cart.length }} items totalling ${{ total }}
-    </div>
+    </FlyOut>
 </template>
 
 <style scoped>
@@ -54,38 +55,6 @@ import ProductCard from '@/components/ProductCard.vue';
         flex-wrap: wrap;
         justify-content: space-around;
     }
-    .card {
-        flex-basis: 15rem;
-        flex-grow: 1;
-        margin: .5rem;
-    }
-    h3 {
-        font-weight: bold;
-    }
-    .price {
-        font-weight: bold;
-        font-size: xx-large;
-        color: hotpink;
-        float: right;
-    }
-
-.flyout {
-    position: fixed;
-    top: 0;
-    right: 0;
-    width: 20rem;
-    height: 100%;
-    background-color: white;
-    border: 1px solid #ccc;
-    box-shadow: -1px 0 5px 0 rgba(0,0,0,.5);
-    z-index: 100;
-    transform: translateX(80%);
-    transition: transform 1s;
-    padding: 1rem;
-}
-.flyout.open, .flyout:hover {
-    transform: translateX(0);
-}
 
 .cart li {
     display: flex;
